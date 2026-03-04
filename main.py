4d6de250b80b4dd87f09ebcf06dc0aa137ba33c6
anac1010 <anaclarabatistaguimaraes479@gmail.com>
2026-03-03 23:14:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+ 
+a = int(input())
+b = int(input())
+
+x = a + b
+
+print(f'X = {x}')

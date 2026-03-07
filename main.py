c5fab39487b9fc37210436f467bad3bee461eca3
anac1010 <anaclarabatistaguimaraes479@gmail.com>
2026-03-06 22:33:32 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-'''
 
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
- 
-a = int(input())
-b = int(input())
-
-x = a + b
-
-print(f'X = {x}')
+a = float(input())
+b = float(input())
+m = (3.5 * a + 7.5 * b) / 11
+print('MEDIA = %.5f'%m)
